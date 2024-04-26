5276dcdfd5d8759e629570f57a8a54365b2aae29
Empirestar247 <ejimofor81@gmail.com>
2024-04-26 06:45:10 +0100

    My fisrttime fullstack software development

--- README.md
+++ README.md
@@ -1,8 +1,130 @@
-# React + Vite
+# Logistic
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# [Django REST framework][docs]
 
-Currently, two official plugins are available:
+[![build-status-image]][build-status]
+[![coverage-status-image]][codecov]
+[![pypi-version]][pypi]
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+**Awesome web-browsable Web APIs.**
+
+Full documentation for the project is available at [https://www.django-rest-framework.org/][docs].
+
+![screentshot]()
+
+# Requirements
+
+* Python (3.12.2)
+* Django (5.0.4)
+* django_cors_headers==4.3.1
+* React + vite
+* "react-router-dom": "^6.22.0",
+* "react-slick": "^0.30.2",
+* "slick-carousel": "^1.8.1"
+* "aos": "^2.3.4",
+* "axios": "^1.6.8",
+* "bootstrap": "^5.3.2",
+
+# Installation
+
+Install using `pip`...
+
+    pip install djangorestframework
+
+Add `'rest_framework'` to your `INSTALLED_APPS` setting.
+
+    INSTALLED_APPS = [
+        ...
+        'rest_framework',
+    ]
+
+# Example
+
+Let's take a look at a quick example of using REST framework to build a simple model-backed API for accessing users and groups.
+
+Startup up a new project like so...
+
+    pip install django
+    pip install djangorestframework
+    django-admin startproject example .
+    ./manage.py migrate
+    ./manage.py createsuperuser
+
+		ow edit the `example/urls.py` module in your project:
+
+```python
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+```
+
+We'd also like to configure a couple of settings for our API.
+
+Add the following to your `settings.py` module:
+
+```python
+INSTALLED_APPS = [
+    ...  # Make sure to include the default installed apps here.
+    'rest_framework',
+]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
+```
+
+That's it, we're done!
+
+    ./manage.py runserver
+
+You can now open the API in your browser at `http://127.0.0.1:8000/`, and view your new 'users' API. If you use the `Login` control in the top right corner you'll also be able to add, create and delete users from the system.
+
+You can also interact with the API using command line tools such as [`curl`](https://curl.haxx.se/). For example, to list the users endpoint:
+
+$ curl -H 'Accept: application/json; indent=4' -u admin:password http://127.0.0.1:8000/users/
+    [
+        {
+            "url": "http://127.0.0.1:8000/users/1/",
+            "username": "admin",
+            "email": "admin@example.com",
+            "is_staff": true,
+        }
+    ]
+
+Or to create a new user:
+
+$ curl -X POST -d username=new -d email=new@example.com -d is_staff=false -H 'Accept: application/json; indent=4' -u admin:password http://127.0.0.1:8000/users/
+    {
+        "url": "http://127.0.0.1:8000/users/2/",
+        "username": "new",
+        "email": "new@example.com",
+        "is_staff": false,
+    }
+
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,85 +1,146 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 function Form() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        subject: '',
-        phone: '',
-        message: '',
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    phone: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
-    const [errors, setErrors] = useState({});
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+  const validateForm = () => {
+    const errors = {};
+    if (!formData.name.trim()) {
+      errors.name = "Name is required";
+    }
 
-    const validateForm = () => {
-        const errors = {};
-        if (!formData.name.trim()) {
-            errors.name = 'Name is required';
-        }
+    if (!formData.email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      errors.email = "Invalid email format";
+    }
+    if (!formData.phone.trim()) {
+      errors.phone = "phone is required";
+    }
 
-        if (!formData.email.trim()) {
-            errors.email = 'Email is required';
-        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
-            errors.email = 'Invalid email format';
-        }
+    if (!formData.subject.trim()) {
+      errors.subject = "Subject is required";
+    }
 
-        if (!formData.subject.trim()) {
-            errors.subject = 'Subject is required';
-        }
+    if (!formData.message.trim()) {
+      errors.message = "Message is required";
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
-        if (!formData.message.trim()) {
-            errors.message = 'Message is required';
-        }
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents default form submission
+    if (validateForm()) {
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/contact/postcontact/",
+          formData
+        );
+        console.log("form submitted", response.data);
+        alert("Message sent successfully");
+        // Page reload
+        window.location.reload();
+      } catch (error) {
+        console.error("Error submitting form", error);
+      }
+      // Processing the information not the endpoint
+      //alert('Message sent successfully')
+      //console.log(formData);
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); // Prevents default form submission
-        if (validateForm()) {
-            // Processing the information not the endpoint
-            alert('Message sent successfully')
-            console.log(formData);
+      // To redirect to another page
+      //window.location.href= 'contact';
+    }
+  };
 
-            // To redirect to another page
-            window.location.href= 'contact';
-        }
-    };
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}> {/* Added onSubmit attribute */}
-                <div className="d-flex mt-3">
-                    <div className="flex-grow-1 me-2">
-                        <input type="text" name="name" value={formData.name} placeholder="Name" className={`form-control ${errors.name && 'is-invalid'}`} onChange={handleChange} />
-                        {errors.name && <div className='invalid-feedback'>{errors.name}</div>}
-                    </div>
-                    <div className="flex-grow-1 ms-2">
-                        <input type="text" name="email" value={formData.email} placeholder="Email" className={`form-control ${errors.email && 'is-invalid'}`} onChange={handleChange} />
-                        {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
-                    </div>
-                </div>
-                <div className="d-flex mt-3">
-                    <div className="flex-grow-1 me-2">
-                        <input type="text" name="phone" value={formData.phone} placeholder="Phone (Optional)" className="form-control" /> {/* Fixed class name */}
-                    </div>
-                    <div className="flex-grow-1 ms-2">
-                        <input type="text" name="subject" value={formData.subject} placeholder="Subject" className={`form-control ${errors.subject && 'is-invalid'}`} onChange={handleChange} />
-                        {errors.subject && <div className='invalid-feedback'>{errors.subject}</div>}
-                    </div>
-                </div>
-                <textarea id="" cols="30" rows="10" className={`form-control mt-3 ${errors.message && 'is-invalid'}`} onChange={handleChange} name="message" value={formData.message} placeholder="Enter your message Here"></textarea> 
-                <button type="submit" className="btn btn-danger mt-3">Submit</button> {/* Added type="submit" */}
-            </form>
-        </>
-    );
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="d-flex mt-3">
+          <div className="flex-grow-1 me-2">
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              placeholder="Name"
+              className={`form-control ${errors.name ? "is-invalid" : ""}`}
+              onChange={handleChange}
+            />
+            {errors.name && (
+              <div className="invalid-feedback">{errors.name}</div>
+            )}
+          </div>
+          <div className="flex-grow-1 ms-2">
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              placeholder="Email"
+              className={`${errors.email ? "form-control is-invalid" : "form-control"}`}
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <div className="invalid-feedback">{errors.email}</div>
+            )}
+          </div>
+        </div>
+        <div className="d-flex mt-3">
+          <div className="flex-grow-1 me-2">
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              placeholder="Phone (Optional)"
+              className={`form-control ${errors.phone && "is-invalid"}`}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex-grow-1 ms-2">
+            <input
+              type="text"
+              name="subject"
+              value={formData.subject}
+              placeholder="Subject"
+              className={`form-control ${errors.subject && "is-invalid"}`}
+              onChange={handleChange}
+            />
+            {errors.subject && (
+              <div className="invalid-feedback">{errors.subject}</div>
+            )}
+          </div>
+        </div>
+        <textarea
+          id=""
+          cols="30"
+          rows="10"
+          className={`form-control mt-3 ${errors.message && "is-invalid"}`}
+          onChange={handleChange}
+          name="message"
+          value={formData.message}
+          placeholder="Enter your message Here"
+        ></textarea>
+        <button type="submit" className="btn btn-danger mt-3">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Form;
